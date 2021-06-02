e0793abb93f75b52d1a039c9a153bedee506700a
Peter Bludau <bludau@fortiss.org>
2021-06-02 19:59:23 +0200

    correct book example

--- python/statement.py
+++ python/statement.py
@@ -6,7 +6,7 @@ def statement(invoice, plays):
     volume_credits = 0
     result = f'Statement for {invoice["customer"]}\n'
 
-    def format_as_dollars(amount):
+    def format(amount):
         return f"${amount:0,.2f}"
 
     for perf in invoice['performances']:
@@ -31,10 +31,10 @@ def statement(invoice, plays):
         if "comedy" == play["type"]:
             volume_credits += math.floor(perf['audience'] / 5)
         # print line for this order
-        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
+        result += f' {play["name"]}: {format(this_amount/100)} ({perf["audience"]} seats)\n'
         total_amount += this_amount
 
-    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
+    result += f'Amount owed is {format(total_amount/100)}\n'
     result += f'You earned {volume_credits} credits\n'
     return result
 
