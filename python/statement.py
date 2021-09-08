1bd7d4bc01af22e1c2eaeab3cb67253ec7eabe78
Jonas Jelten <jj@sft.lol>
2021-09-08 15:02:37 +0200

    simple restructuring variant

--- python/statement.py
+++ python/statement.py
@@ -1,41 +1,81 @@
-import math
+"""
+simple restructuring to be extensible for different play types.
+
+if things need to get more complicated, the PERFORMANCES dict
+should become a class, and one could even start inheritance then.
+"""
 
 
-def statement(invoice, plays):
+def tragedy_price(audience_size):
+    amount = 40000
+    if audience_size > 30:
+        amount += 1000 * (audience_size - 30)
+    return amount
+
+
+def comedy_price(audience_size):
+    amount = 30000
+    if audience_size > 20:
+        amount += 10000 + 500 * (audience_size - 20)
+
+    amount += 300 * audience_size
+    return amount
+
+
+PERFORMANCES = {
+    "tragedy": {
+        "price": tragedy_price,
+        "volume_credits": lambda _: 0,
+    },
+    "comedy": {
+        "price": comedy_price,
+        "volume_credits": lambda audience: audience // 5,
+    },
+}
+
+
+def calculate_price(invoice, plays):
     total_amount = 0
     volume_credits = 0
-    result = f'Statement for {invoice["customer"]}\n'
-
-    def format(amount):
-        return f"${amount:0,.2f}"
+    play_prices = list()
 
     for perf in invoice['performances']:
         play = plays[perf['playID']]
-        if play['type'] == "tragedy":
-            this_amount = 40000
-            if perf['audience'] > 30:
-                this_amount += 1000 * (perf['audience'] - 30)
-        elif play['type'] == "comedy":
-            this_amount = 30000
-            if perf['audience'] > 20:
-                this_amount += 10000 + 500 * (perf['audience'] - 20)
+        audience = perf['audience']
 
-            this_amount += 300 * perf['audience']
+        prices = PERFORMANCES.get(play['type'])
+        if prices is None:
+            raise ValueError(f'unknown performance type: {play["type"]}')
 
-        else:
-            raise ValueError(f'unknown type: {play["type"]}')
+        # base price + extra price depending on audience size
+        this_amount = prices['price'](audience)
 
-        # add volume credits
-        volume_credits += max(perf['audience'] - 30, 0)
-        # add extra credit for every ten comedy attendees
-        if "comedy" == play["type"]:
-            volume_credits += math.floor(perf['audience'] / 5)
+        # generate volume credits, starting at 30 watchers
+        volume_credits += max(audience - 30, 0)
+
+        # extra atendee credits
+        volume_credits += prices['volume_credits'](audience)
+
         # print line for this order
-        result += f' {play["name"]}: {format(this_amount/100)} ({perf["audience"]} seats)\n'
+        play_prices.append((play['name'], this_amount, audience))
         total_amount += this_amount
 
-    result += f'Amount owed is {format(total_amount/100)}\n'
-    result += f'You earned {volume_credits} credits\n'
-    return result
+    return play_prices, volume_credits, total_amount
 
 
+def format_as_dollars(amount):
+    return f"${amount/100:0,.2f}"
+
+
+def statement(invoice, plays):
+    result = list()
+    result.append(f'Statement for {invoice["customer"]}')
+
+    play_prices, volume_credits, total_amount = calculate_price(invoice, plays)
+    for name, amount, audience in play_prices:
+        result.append(f' {name}: {format_as_dollars(amount)} '
+                      f'({audience} seats)')
+
+    result.append(f'Amount owed is {format_as_dollars(total_amount)}')
+    result.append(f'You earned {volume_credits} credits')
+    return "\n".join(result)
